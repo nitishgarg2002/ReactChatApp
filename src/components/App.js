065b6892ba1b60831806044e88bea7b8c6580a9d
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-04-26 22:14:00 +0530

    initial changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className="App">App</div>;
-}
+import React from 'react';
 
+class App extends React.Component {
+  render() {
+    console.log('props', this.props);
+    return <div>App</div>;
+  }
+}
 export default App;
--- src/reducers/posts.js
+++
@@ -1,3 +0,0 @@
-export default function posts(state = []) {
-  return state;
-}
