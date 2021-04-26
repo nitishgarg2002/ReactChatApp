7e547a13e97c7b08520393cb087b758199c550d1
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-04-26 23:20:12 +0530

    chatfeed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
 import React from 'react';
+import ChatFeed from './ChatFeed';
+import './App.css';
 
-class App extends React.Component {
-  render() {
-    console.log('props', this.props);
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="c20c9348-0bb8-412d-a04c-2469206b3585"
+      userName="nitish"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
 export default App;
