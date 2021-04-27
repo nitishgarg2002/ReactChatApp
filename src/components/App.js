9b0c5357e152a02a81c34b0f956efa3992e0310e
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-04-27 15:20:17 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,11 @@ const App = () => {
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3'
+        ).play()
+      }
     />
   );
 };
